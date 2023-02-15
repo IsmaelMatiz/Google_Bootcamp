a29f7198ac40bac30d32593c91faaa3c011e661c
IsmaelMatiz <ismaelmatiz@gmail.com>
2023-02-15 10:15:23 -0500

    Presentation Card Finished

--- Android_Basics_With_Compose/PresentationCard/app/src/main/java/com/example/presentationcard/MainActivity.kt
+++ Android_Basics_With_Compose/PresentationCard/app/src/main/java/com/example/presentationcard/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.presentationcard
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,12 +28,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.presentationcard.ui.theme.PresentationCardTheme
 import com.example.presentationcard.ui.theme.PurpleSmile
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PresentationCardTheme {
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setStatusBarColor(
+                        color = Color.Blue,
+                        darkIcons = false
+                    )
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -48,7 +62,15 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                         Row(modifier = Modifier.weight(1f)) {
-
+                            Column {
+                                NameAndProfession(name = "Ismael Matiz", profession = "Android Developer. ")
+                                Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
+                                    SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.phone_number))
+                                    SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.email))
+                                    SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.github_profile))
+                                }
+                                ButtonSeeMore()
+                            }
                         }
                     }
                 }
@@ -80,7 +102,9 @@ fun ProfilePic(picture: Painter){
 @Composable
 fun NameAndProfession(name: String, profession: String){
     Column(
-        modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 80.dp),
         horizontalAlignment = Alignment.CenterHorizontally
         )
         {
@@ -100,13 +124,34 @@ fun NameAndProfession(name: String, profession: String){
 }
 
 @Composable
-fun SocialField(phone: String, email: String, github:String){
-
+fun SocialField(symbol: Painter, contactInfo: String){
+    Row(modifier = Modifier.padding(8.dp)) {
+        Row(modifier = Modifier
+            .border(BorderStroke(2.dp, Color.Black), shape = RoundedCornerShape(30))
+            .fillMaxWidth()
+            .padding(6.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = contactInfo)
+        }
+    }
 }
 
 @Composable
 fun ButtonSeeMore() {
-
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                colorResource(id = R.color.dark_blue)
+            )
+        ) {
+            Text(text = "See More",
+                color = Color.White
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -128,7 +173,15 @@ fun DefaultPreview() {
                 }
             }
             Row(modifier = Modifier.weight(1f)) {
-                NameAndProfession(name = "Ismael Matiz", profession = "Android Developer. ")
+                Column {
+                    NameAndProfession(name = "Ismael Matiz", profession = "Android Developer. ")
+                    Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
+                        SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.phone_number))
+                        SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.email))
+                        SocialField(symbol = painterResource(id = R.drawable.ic_launcher_foreground), contactInfo = stringResource(id = R.string.github_profile))
+                    }
+                    ButtonSeeMore()
+                }
             }
         }
     }
